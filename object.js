8f3a0ce232c8044d555edde21589f2b3aa7fa9a4
developerlenin <justlenin9095@gmail.com>
2022-08-30 17:18:47 +0530

    new

---
+++ object.js
@@ -0,0 +1,41 @@
+{
+var scroe = {
+	tamil : 85,
+	english : 90,
+	maths :90,
+	sicence :75,
+	social :90,
+	
+	  avg :function () {  
+		 let find= (this.tamil +this.english+this.maths +this.sicence +this.social)/5
+		 consol(find);
+}
+ 
+
+}
+scroe.avg()
+}
+{
+let preson = {
+	name: "lenin",
+	age : 24,
+	gender : "male"
+	
+}
+	document.write(preson.age)
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
